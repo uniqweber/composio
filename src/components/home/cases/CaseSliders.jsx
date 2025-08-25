ac78d59c0c5fc850d176818c683226ef3c848f97
Rayan Ahmed <uniqweber@gmail.com>
2025-08-25 16:41:55 +0600

    case studies section added

---
+++ src/components/home/cases/CaseSliders.jsx
@@ -0,0 +1,48 @@
+import waterMark from "@/assets/images/case-studies-card-watermark.png";
+import bg from "@/assets/images/case-studies-card-bg.png";
+
+export function TestimonialCard({caseStudy}) {
+    return (
+        <div className=" max-w-xl xl:max-w-3xl  p-6 md:px-20 md:pt-20 md:pb-28 mx-auto relative group">
+            <img
+                src={bg}
+                alt=""
+                className=" lg:group-hover:opacity-100 opacity-20 size-full object-cover xl:size-auto xl:-top-10 xl:-left-10  absolute inset-0 duration-500"
+            />
+            <img
+                src={waterMark}
+                alt=""
+                className="scale-72 xl:scale-80 top-1/2 left-1/2 -translate-1/2 xl:-ml-17 xl:mt-5  lg:group-hover:opacity-0 duration-500 opacity-15 absolute inset-0 "
+            />
+            <div className="relative  z-10 max-w-lg  xl:max-w-[519px] w-full ">
+                <h3 className="text-black/40 font-roobert font-light mb-5">{caseStudy.name}</h3>
+
+                <div className="mb-10">
+                    <p className="text-3xl xl:text-4xl font-ibm font-light text-black/60 leading-11 xl:leading-12">{caseStudy.title}</p>
+                </div>
+
+                <div className="flex items-center font-roobert gap-4 mb-10">
+                    <img src={caseStudy.logo} alt="user" className="size-6 " />
+                    <div>
+                        <h4 className="text-black/60 ">{caseStudy.user}</h4>
+                        <p className="text-xs text-black/40 font-light">{caseStudy.role}</p>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-5 font-roobert">
+                    {caseStudy.highlights.map((highlight) => (
+                        <div className="">
+                            <div className="text-2xl xl:text-4xl font-bold text-black mb-2">
+                                {highlight.infoTitle}{" "}
+                                {highlight.unit && (
+                                    <span className="text-xl xl:text-2xl font-normal text-black/40">{highlight.unit}</span>
+                                )}
+                            </div>
+                            <p className="text-black/40 mx-auto  font-roobert-mono xl:text-xl ">{highlight.infoContent}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Benefits from "@/components/home/benefits";
 import BuiltForAgents from "@/components/home/built";
+import CaseStudies from "@/components/home/cases";
 import PriceCompare from "@/components/home/compare";
 import Intelligence from "@/components/home/intelligence";
 import Tweets from "@/components/home/tweets";
@@ -7,6 +8,7 @@ import Tweets from "@/components/home/tweets";
 export default function Home() {
     return (
         <>
+            <CaseStudies />
             <BuiltForAgents />
             <Tweets />
             <Benefits />
